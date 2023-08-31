9f326254097cadc674e6f94fe5364f7f4ac1423e
giszajic <gisela.zajic@mercadolibre.com>
2023-08-31 13:39:50 -0300

    Adding func_promedio_ej2.go

--- program_practice/estructuras_control/func_promedio_ej2.go
+++ program_practice/estructuras_control/func_promedio_ej2.go
@@ -1,1 +1,38 @@
+/*
+Un colegio necesita calcular el promedio (por estudiante) de sus calificaciones. Se solicita generar una función
+en la cual se le pueda pasar N cantidad de enteros y devuelva el promedio. No se pueden introducir notas negativas.
+*/
+
 package main
+
+import "fmt"
+
+func calcularPromedio(notas []int) float32 {
+
+	//evalua que no se ingresen notas negativas
+	var suma int
+	for _, nota := range notas {
+		if nota < 0 {
+			panic("No se permiten notas negativas")
+		}
+		suma += nota
+	}
+
+	//caso de cantidad de notas = 0
+	cantidadNotas := len(notas)
+	if cantidadNotas == 0 {
+		return 0.0 //
+	}
+
+	promedio := float32(suma) / float32(cantidadNotas)
+	return promedio
+
+}
+
+func main() {
+
+	notas := []int{8, 3, 10, 7, 6, 10}
+	promedio := calcularPromedio(notas)
+
+	fmt.Println("El promedio del alumno es: ", promedio)
+}
